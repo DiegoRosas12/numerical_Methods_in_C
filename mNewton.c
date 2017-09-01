5b99529639d53e2a9ea2942ecdd6577784ca12db
TheCoreExT <de.rosasgonzalez@ugto.mx>
2017-08-31 22:19:26 -0500

    cambios a mnewton

--- mNewton.c
+++ mNewton.c
@@ -1,9 +1,22 @@
+/*Universidad de Guanajuato
+Division de Ingenierias Campus Irapuato Salamanca
+Metodos Numericos
+Diego Eduardo Rosas Gonzalez
+*/
 #include <stdio.h>
 #include <math.h>
 
+float f(float xi) {
+    return (powf(xi,2.0) - (cos(xi)) -1);
+}
+
+float df(float xi) {
+    return ((2*xi) + sin(xi));
+}
+
 int main() {
-    int i;
-    float a, b, xi, fxi, dfxi, er;
+    int i=0;
+    float a, b, xi, fxi, dfxi, xi1, xi1a, er=0;
     printf("F(x) = x²-cos(x)-1\nF'(x) = 2x+sin(x)\n");
     printf("Ingrese el valor minimo del rango: ");
     scanf("%f",&a);
@@ -14,14 +27,22 @@ int main() {
         a = b - a;
         b = b - a;
     }
+    printf("\nminimo: %.1f\tmaximo: %.1f\n",a,b);
+    printf(" i|  xi  | f(xi)|f'(xi)| xi+1 |  Er  |\n");   
     xi = (a + b) / 2;
     do {
-        fxi = (fpow(xi,2)) - (cos(xi)) -1;
-        dfxi = (2*xi) + sin(xi);
-        xi = xi - (fxi / dfxi);
+        fxi = f(xi);
+        dfxi = df(xi);
+        xi1a = xi - (fxi / dfxi);
+        printf("%02i|%.4f|%.4f|%.4f|%.4f|%.4f|\n",i,xi,fxi,dfxi,xi1a,er);
         i++;
-    }while(i<20);
-
+        xi = xi1a;
+        xi1 = xi - (f(xi) / df(xi));
+        er = fabs(fabs(xi1a) - fabs(xi1));
+    }while((er<-0.000001)||(er>0.000001));
+    printf(" %01i|%.4f|%.4f|%.4f|%.4f|%.4f|\n",i,xi,fxi,dfxi,xi1a,er);
+    printf("\nEl valor del punto medio es %.4f y el numero de iteraciones fue %d.\n",xi1,i);
+    
     getchar();
     getchar();
     return 0;
