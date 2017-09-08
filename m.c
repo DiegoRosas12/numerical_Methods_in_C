87e264b9791212e5edbcc20a01054861c1723fd8
TheCoreExT <de.rosasgonzalez@ugto.mx>
2017-09-07 20:57:15 -0500

    final

---
+++ m.c
@@ -0,0 +1,46 @@
+#include<conio.h> 
+#include<math.h> 
+#include<stdlib.h> 
+#include<stdio.h> 
+#include<iostream.h> 
+
+double f(float Xi); 
+double f1(float Xr); 
+
+int main(void){ 
+int SI=1; 
+do{ 
+system("cls" ); 
+float Xi=0,Ea=0,Xz=0,Xr=0; 
+int cont=0; 
+printf("ttPRACTICA #5n" ); 
+printf("t METODO DE LA SECANTEn" ); 
+printf("tbasado en una practica de salonnn" ); 
+printf("instroduce Xi:n" ); 
+scanf("%f",&Xi); 
+printf("nnI Xi-1 Xi Xi+1 f(Xi-1) f(Xi) Ean" ); 
+
+do{ 
+Xr=(Xi-1); 
+Xz= Xi-((f(Xi)*(Xr-Xi))/(f1(Xr)-f(Xi))); 
+Ea=fabs((Xz-Xi)/Xz)*100; 
+printf("%d %f %.5f %.5f %.5f %.5f %fn",cont,Xr,Xi,Xz,f1(Xr),f(Xi),Ea); 
+cont++; 
+Xi=Xz; 
+}while(Ea>=0.05); 
+printf("nnn" ); 
+printf("DESEAS REALIZAR LA OPERACION OTRA VEZ? SI[1] NO[2]n" ); 
+scanf("%d",&SI); 
+}while(SI<=1); 
+system("PAUSE" ); 
+} 
+
+double f(float Xi){ 
+return (exp(-Xi))-Xi; 
+} 
+
+
+double f1(float Xr){ 
+float n = (exp(-Xr))-Xr; 
+return n; 
+} 
--- secante.c
+++ secante.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 float f(float x){
     return (powf(x,20.0)-1);
@@ -6,17 +7,24 @@ float f(float x){
 
 int main() {
     int i=1;
-    float xmenos, xi, fmenos, fxi, xi1, e;
+    float xmenos, xi, fmenos, fxi, xmas, er=0, xmas2;
     printf("Introduzca el valor de X0: ");
-    scanf("%f",&xi1);
+    scanf("%f",&xmenos); 
     printf("Introduzca el valor de X1: ");
     scanf("%f",&xi);
-    printf("i|Xi-1|Xi|f(Xi-1)|f(Xi)|Xi+1|Er|\n");
+    printf("Xo = %.4f\tXi = %.4f\n",xmenos,xi);
+    printf("i |  Xi-1 |   Xi  |f(Xi-1)| f(Xi) | Xi+1  |   Er   |\n");
     do{
-        fmenos = f();
+        fmenos = f(xmenos);
         fxi = f(xi);
-        printf("%i|%f|%f|%f|%f|%f|%f|\n",i,xmenos,xi,fmenos,fxi,xi1,e);
-    }while(i<10);
-
+        xmas = (xi) - ((fxi*(xi-xmenos)) / (fxi-fmenos));
+        printf("%02i|% .4f|% .4f|% .4f|% .4f|% .4f|% 02.4f%%|\n",i,xmenos,xi,fmenos,fxi,xmas,er);
+        xmenos = xi;
+        xi = xmas;
+        xmas2 = (xi) - ((fxi*(xi-xmenos)) / (fxi-fmenos));
+        er = fabs(((xmas2-xmas) / xmas) * 100);
+        i++;
+    }while((er<-0.000001)||(er>0.000001));
+    printf("El valor de Xi+1 es: %.4f y el numero de iteraciones fue %i.\n",xmas,i);
     return 0;
 }
