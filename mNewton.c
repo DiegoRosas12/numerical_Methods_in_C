8a04ee870ae48343fa2fd7d1e5f7b2aa18cb7d2a
de.rosasgonzalez <de.rosasgonzalez@ugto.mx>
2018-10-21 01:17:12 -0500

    set the licence file

--- mNewton.c
+++ mNewton.c
@@ -1,3 +1,24 @@
+/**
+ * Copyright (C) 2018 Diego Eduardo Rosas González
+ * 
+ * This file is part of puntoFijo.
+ * 
+ * puntoFijo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * puntoFijo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with puntoFijo.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+
 /*Universidad de Guanajuato
 Division de Ingenierias Campus Irapuato Salamanca
 Metodos Numericos
